4d0858a9f7f833d1a157270d926517381c752769
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-07-25 13:09:26 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int maxh= 0;
+        for (int i=0; i<height.size(); i++){
+            if (height[i]>maxh) maxh= height[i];
+        }
+        vector<vector<int>> dp(maxh, vector<int>(height.size(), 0));
+        for (int i=0; i<maxh; i++){
+            for (int j=0; j<height.size(); j++){
+                dp[i][j]=0;
+            }
+        }
+        int state= 0; //0 means didn't start, 1 means 1 found
+        for (int i=0; i<height.size(); i++){
+            for (int j=maxh-1; j>maxh-1-height[i]; j--){
+                dp[j][i]= 1;
+            }    
+        }
+        int v=0; 
+        for (int i=0; i<maxh; i++){
+            state=0;
+            int count=0;
+            for (int j=0; j<height.size(); j++){
+                if (dp[i][j]==1){
+                    switch (state){
+                        case 0: count=0; state=1; break;
+                        case 1: v=v+count; count=0; state=1; break;
+                        default: break;
+                    }
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+        return v;
+    }
+};
