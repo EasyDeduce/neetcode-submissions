7369c13177e74d91c056118a23345ca30b19b139
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-07-31 10:41:22 +0530

    Add: sort-colors - submission-18

---
+++ Data Structures & Algorithms/sort-colors/submission-18.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l= 0
+        r= len(nums)-1
+        for i in range(l,r+1):
+            if nums[l]==2 and l<r:
+                t= nums[l]
+                nums[l]=nums[r]
+                nums[r]=t
+                r-=1
+            if  nums[r]==0 and l<r:
+                t= nums[l]
+                nums[l]=nums[r]
+                nums[r]=t
+                l+=1
+            if i>=l and i<len(nums) and nums[i]==0:
+                t= nums[l]
+                nums[l]=nums[i]
+                nums[i]=t
+                l+=1
+            if i>=0 and i<=r and nums[i]==2 :
+                t= nums[r]
+                nums[r]=nums[i]
+                nums[i]=t
+                r-=1
