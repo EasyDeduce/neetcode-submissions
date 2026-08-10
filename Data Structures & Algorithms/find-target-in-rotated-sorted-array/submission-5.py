a2a2666cb2e9438830575f84541e5d748a2ea59a
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-08-11 01:41:49 +0530

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def binary(self, nums:List[int], target:int, start:int, end:int, rot:int)->int:
+        s_t= max((len(nums)+start-rot)%len(nums), start-rot)
+        e_t= max((len(nums)+end-rot)%len(nums), end-rot)
+        if (s_t==e_t):
+            if (nums[start]==target):
+                return start
+            else:
+                return -1
+        
+        if (nums[start]==target):
+            return start
+        if (nums[end]==target):
+            return end
+        
+        mid_t= int((s_t+e_t)/2)
+        mid= (mid_t+rot)%len(nums)
+        if (nums[mid]==target):
+            return mid
+        elif (nums[mid]<target):
+            return self.binary(nums,target, (mid+1)%len(nums), end, rot)
+        else:
+            return self.binary(nums, target, start, mid, rot)
+
+    def search(self, nums: List[int], target: int) -> int:
+        if (nums[0]<nums[-1]):
+            return self.binary(nums,target,0,len(nums)-1,0)
+        j=0
+        if(len(nums)==1):
+            if nums[0]==target: return 0
+            else: return -1
+        while (nums[j]<nums[j+1]):
+            j+=1
+        return self.binary(nums,target,j+1,j,j+1)
