94d0fee0e8eace70657195b262c0f3e13b11b110
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-08-13 01:10:31 +0530

    Add: max-area-of-island - submission-10

---
+++ Data Structures & Algorithms/max-area-of-island/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dfs(self, grid:List[List[int]], i:int ,j:int)->int:
+        if grid[i][j]==0:
+            return 0
+        grid[i][j]=0
+        return 1+self.dfs(grid,min(len(grid)-1,i+1),j)+self.dfs(grid,i,min(len(grid[0])-1,j+1))+self.dfs(grid,max(0,i-1),j)+self.dfs(grid,i,max(0,j-1))
+
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_a=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if(grid[i][j]==0):
+                    continue
+                a= self.dfs(grid,i,j)
+                if (a>max_a):
+                    max_a=a
+        return max_a
