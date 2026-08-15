2a31597c1a66f1bbfa410f7ce341b1fb8449ac5c
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-08-15 23:24:51 +0530

    Add: maximum-frequency-after-subarray-operation - submission-16

---
+++ Data Structures & Algorithms/maximum-frequency-after-subarray-operation/submission-16.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxFrequency(self,nums:List[int],k:int)->int:
+        initf= nums.count(k)
+        if initf==0:
+            freq={}
+            for i in nums:
+                freq[i]=freq.get(i,0)+1
+            return max(freq.values())
+
+        t=set()
+        for i in nums:
+            if i not in t and i!=k:
+                t.add(i)
+        mf=-1
+        for i in t:
+            f=initf
+            for iter in range(len(nums)):
+                if nums[iter]==i:
+                    f=f+1
+                elif nums[iter]==k:
+                    f=max(initf,f-1)
+                if f>mf:
+                    mf=f
+        return mf
