c6fde0a186eae82bc5eacfcb8e8caeb1e73a8dfc
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-08-23 18:00:00 +0530

    Add: n-queens-ii - submission-2

---
+++ Data Structures & Algorithms/n-queens-ii/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        posDiag=set()
+        negDiag=set()
+        col=set()
+        count=0
+        def  backtrack(r):
+            nonlocal count
+            if r==n:
+                count+=1
+                return
+            for c in range(n):
+                if c in col or r-c in negDiag or r+c in posDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c) 
+                backtrack(r+1)
+                col.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+        backtrack(0)
+        return count
