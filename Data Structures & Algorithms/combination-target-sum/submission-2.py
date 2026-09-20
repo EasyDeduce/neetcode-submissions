ee4a8b39e228b0aa7a65e7af2190d17b44cadb41
EasyDeduce <143724304+EasyDeduce@users.noreply.github.com>
2026-09-21 00:55:13 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:  
+        l=[]
+        temp=[]
+        def dfs(i,s):
+            if s==target:
+                l.append(temp.copy())
+                return
+            elif s>target:
+                return  
+            for j in range(i,len(nums)):
+                temp.append(nums[j])
+                s+=nums[j]
+                dfs(j,s)
+                s-=nums[j]
+                temp.pop()
+        dfs(0,0)
+        return l
